5562378d0223d6c1024227c45895b56db9226536
trex0r <68485979+trex0r@users.noreply.github.com>
2026-03-09 05:14:23 +0000

    Update entrypoint.sh

--- docker/scripts/entrypoint.sh
+++ docker/scripts/entrypoint.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -e
 
-if [ "$DOWNLOAD_MODEL" = "true" ]; then
-    python download_model.py --output api/src/models/v1_0
+if [ ! -f /app/api/src/models/v1_0/kokoro-v1_0.pth ]; then
+  echo "Model not found, downloading..."
+  python download_model.py --output /app/api/src/models/v1_0
 fi
 
 exec uv run --extra $DEVICE --no-sync python -m uvicorn api.src.main:app --host 0.0.0.0 --port 8880 --log-level debug
